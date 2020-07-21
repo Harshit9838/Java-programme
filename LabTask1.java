2d6cd87c181bd070656ab138b0f86f98b3e74470
Harshit Dixit <harshit.dixit_cs19@gla.ac.in>
2020-07-21 23:18:01 +0530

    Add files via upload
    Print Hello World

---
+++ LabTask1.java
@@ -0,0 +1,11 @@
+//Author: Harshit Dixit
+
+package lab;
+
+// task 1- Create a "hello, World!" program in Java.
+
+class LabTask1 {
+    public static void main (String[] args) {
+        System.out.println("Hello , World!");
+    }
+}
