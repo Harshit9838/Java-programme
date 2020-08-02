728f97205b0f80442058ae310cfc9aded973927f
Harshit Dixit <harshit.dixit_cs19@gla.ac.in>
2020-08-02 23:06:23 +0530

    Add files via upload

---
+++ BuzzFizz.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class BuzzFizz {
+    public static void main(String[] args) {
+        Scanner scannerObject = new Scanner(System.in);
+        int lastNumber = scannerObject.nextInt();
+        scannerObject.close();
+        for (int number = 1; number <= lastNumber; number++) {
+            boolean numberIfDivisibleByThree = (number % 3 == 0);
+            boolean numberIfDivisibleByFive = (number % 5 == 0);
+            if (numberIfDivisibleByThree && numberIfDivisibleByFive) {
+                System.out.print("fizzbuzz" + (number <= lastNumber - 1 ? ", " : " . "));
+            } else if (numberIfDivisibleByThree) {
+                System.out.print("fizz" + (number <= lastNumber - 1 ? ", " : ". "));
+            } else if (numberIfDivisibleByFive) {
+                System.out.print("buzz" + (number <= lastNumber - 1 ? ", " : ". "));
+            } else {
+                System.out.print("buzz" + (number <= lastNumber - 1 ? ", " : ". "));
+            }
+        }
+    }
+}
