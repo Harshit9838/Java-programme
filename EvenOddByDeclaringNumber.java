bccdc0417c0ebcf76a50dd44325684f84bc9947b
Harshit Dixit <harshit.dixit_cs19@gla.ac.in>
2021-05-02 23:52:29 +0530

    Add files via upload

---
+++ EvenOddByDeclaringNumber.java
@@ -0,0 +1,19 @@
+/*Created by Intellij IDEA
+ * Author: Harshit Dixit(Harshit9838)
+ * Date: 02-05-2021
+ *Time: 10:59 PM
+ */
+
+package Basics;
+
+public class EvenOddByDeclaringNumber {
+    public static void main(String[] args) {
+        int x=13;
+        if(x%2==0){
+            System.out.println("Even");
+        }
+        else{
+            System.out.println("odd");
+        }
+    }
+}
